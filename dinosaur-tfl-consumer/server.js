7b2f163ea96e2e2cb266d64d3a64cefb3e3d0412
Joaquin Corchero <joaquin.corchero@gmail.com>
2018-01-15 23:15:07 +0000

    Added new route to return tfl data

--- dinosaur-tfl-consumer/server.js
+++ dinosaur-tfl-consumer/server.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var port = 8090;
 
 var dinoServiceUrl = 'http://localhost:8080/api/dinosaurs';
-var tflServiceUrl = 'https://api.tfl.gov.uk';
+var tflServiceUrl = 'https://api.tfl.gov.uk/journey/journeyresults/westminster/to/bank';
 
 router.get('/dinosaurs', function (req, res) {
     request.get(dinoServiceUrl, function (error, response, body) {
@@ -40,9 +40,44 @@ router.get('/dinosaurs', function (req, res) {
     });
 });
 
+router.get('/tfl', function (req, res) {
+    request.get(tflServiceUrl, function (error, response, body) {
+        if(error != null){
+            res.send(`There was a problem with the request!: ${error}}` );
+            return;
+        };
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+        var data = JSON.parse(body);
+
+        var html = `<html>
+            <head>
+            <title>tfl</title>
+            </head>
+            <body>
+            <h1>Some tfl data</h1>
+            <h2>To:</h2>
+            <p>
+            ${data.toLocationDisambiguation.disambiguationOptions[0].place.commonName}
+            </p>
+            <h2>From:</h2>
+            <p>
+            ${data.fromLocationDisambiguation.disambiguationOptions[0].place.commonName}
+            </p>
+            <h2>Recomended max age minutes:</h2>
+            <p>
+            ${data.recommendedMaxAgeMinutes}
+            </p>
+            </body></html>`;
+
+        res.send(html);
+    });
+});
+
 
 
 app.use('/', router);
 app.listen(port);
 
-console.log(`Consumer-A is listening to port ${port}`);
+console.log(`dinosaur-tfl-consumer is listening to port ${port}`);
