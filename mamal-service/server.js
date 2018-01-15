69e8387c4997078c7ddbad4f6c25a027a1e3a0dd
Joaquin Corchero <joaquin.corchero@gmail.com>
2018-01-15 22:23:09 +0000

    Added mamal service

---
+++ mamal-service/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+var router = express.Router();
+var port = 8085;
+
+router.get('/', function(req, res){
+    var result = {
+        resultCount: 20,
+        message: req.query.message
+    };
+    console.log(result);
+    res.json(result);
+});
+
+app.use('/api', router);
+app.listen(port);
+
+console.log('Provider-B listning to port ' + port);
