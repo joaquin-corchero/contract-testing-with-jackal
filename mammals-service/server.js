cd095149fb87e8ede8099211620b21a3062f27cb
Joaquin Corchero <joaquin.corchero@thetrainline.com>
2018-01-26 09:16:07 +0000

    Addition of paged get

--- mammals-service/server.js
+++ mammals-service/server.js
@@ -13,23 +13,38 @@ app.listen(port);
 
 console.log('Mammals Service listening to port ' + port);
 
+const getPaged = function (pageNumber, pageSize) {
+    return {
+        data: mammals.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize),
+        totalNumber: mammals.length
+    };
+};
+
 app.get('/api/mammals/:id', function (req, res, next) {
     console.log('GETS HERE');
     const result = mammals[req.params.id * 1];
     res.json(result);
 });
 
-app.get('/api/mammals/', function (req, res) {
+//localhost:8085/api/mammals/0/5 v1
+app.get('/api/mammals/:pageNumber?/:pageSize?', function (req, res) {
+    const version = req.get("version");
+    if (version == "1") {
+        const pageNumber = req.params.pageNumber * 1;
+        const pageSize = req.params.pageSize * 1;
+        res.json(getPaged(pageNumber, pageSize));
+        return;
+    }
     res.json(mammals);
 });
 
 app.post('/api/mammals', function (req, res) {
     const { familyName: familyName, familyCommonName: familyCommonName, speciesUrl: speciesUrl } = req.body;
-    if (!name || !diet || (extinct !== true && extinct !== false)) {
+    if (!familyName || !familyCommonName || !speciesUrl) {
         res.sendStatus(400);
         return;
     }
 
-    mammals.push({familyName: familyName, familyCommonName: familyCommonName, speciesUrl: speciesUrl });
+    mammals.push({ id: mammals.length, familyName: familyName, familyCommonName: familyCommonName, speciesUrl: speciesUrl });
     res.sendStatus(201);
 });
